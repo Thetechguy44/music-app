068def6d9db140a68d1ba74e691cd6a1aea8b3fe
Dev Gana <yahaya128173908@gmail.com>
2024-10-06 17:35:03 +0100

    add profile data to the livewire component

--- app/Livewire/Backend/Profile.php
+++ app/Livewire/Backend/Profile.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Backend;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class Profile extends Component
 {
@@ -19,10 +20,36 @@ class Profile extends Component
     public function mount()
     {
         $this->tab = request()->tab ? request()->tab : $this->tabname;
+
+        $profile = Auth::user();
+
+        // Assign basic profile data
+        $this->id = $profile->id;
+        $this->name = $profile->name;
+        $this->email = $profile->email;
+        $this->phone = $profile->phone;
+        $this->dob = $profile->dob;
+        $this->gender = $profile->gender;
+        $this->address = $profile->address;
+        $this->created_on = $profile->created_on;
+
+        // Check if the artist relationship exists
+        if ($profile->artist) {
+            $this->bio = $profile->artist->bio;
+            $this->image = $profile->artist->image;
+            $this->artist_name = $profile->artist->artist_name;
+        } else {
+            // Handle the case when the artist relation is null (optional)
+            $this->bio = null;
+            $this->image = null;
+            $this->artist_name = null;
+        }
     }
 
     public function render()
     {
         return view('livewire.backend.profile');
     }
+
+    
 }
